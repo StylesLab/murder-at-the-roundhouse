0db28e348bedf9c421b81f482f2106ff30ed0c24
StylesLab <bjsinnott@hotmail.com>
2026-07-31 16:49:39 +0300

    Add Roundhouse room sequence

---
+++ highstakes-oldmaid/rooms.js
@@ -0,0 +1,14 @@
+"use strict";
+
+window.HIGH_STAKES_ROOMS = [
+  { name: "Main Hall", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/mainhall.png", narrative: "The front doors slam shut. Somewhere beyond the chandelier, a floorboard creaks." },
+  { name: "Drawing Room", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/drawingroom.png", narrative: "A half-finished drink trembles beside the cold fireplace." },
+  { name: "Library", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/library.png", narrative: "A book lies open at a page someone did not want found." },
+  { name: "Dining Room", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/diningroom.png", narrative: "One place setting has been disturbed, though dinner ended hours ago." },
+  { name: "Kitchen", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/kitchen.png", narrative: "The servants' bell rings once, though no one is there to pull it." },
+  { name: "Orangery", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/orangery.png", narrative: "Rain taps the glass as muddy footprints vanish among the palms." },
+  { name: "Music Room", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/musicroom.png", narrative: "A single piano key sounds in the dark." },
+  { name: "Guest Bedroom", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/guestbedroom.png", narrative: "A suitcase has been searched and carefully closed again." },
+  { name: "Attic", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/rooms/attic.png", narrative: "Dust hangs in the air around a trunk bearing a broken lock." },
+  { name: "Final Confrontation", image: "https://raw.githubusercontent.com/StylesLab/SquaredThings/matr_3_2/MATR/MATR.Game/wwwroot/images/map.png", narrative: "The guests return to the map table. One name remains on each detective's board." }
+];
